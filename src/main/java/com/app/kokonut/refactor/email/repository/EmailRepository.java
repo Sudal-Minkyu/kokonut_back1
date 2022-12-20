e3d9698226e5ddf9435f76150b72502e9d9c9d23
joy <joy@kokonut.me>
2022-12-20 18:00:34 +0900

    사용하지 않는 자동생성 파일 삭제

--- src/main/java/com/app/kokonut/refactor/email/repository/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.kokonut.refactor.email.repository;
-
-import com.app.kokonut.refactor.email.entity.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface EmailRepository extends JpaRepository<Email, Integer>, JpaSpecificationExecutor<Email> {
-
-}
