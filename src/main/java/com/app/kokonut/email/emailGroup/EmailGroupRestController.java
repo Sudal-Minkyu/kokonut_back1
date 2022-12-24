1fd54b431f31756fc4f7481704a7a2bed1a17a33
joy <joy@kokonut.me>
2022-12-24 16:00:16 +0900

    emailGroup 리팩토링 준비

--- src/main/java/com/app/kokonut/email/emailGroup/EmailGroupRestController.java
+++ src/main/java/com/app/kokonut/email/emailGroup/EmailGroupRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "")
 @Validated
 @RestController
-@RequestMapping("/emailGroup")
+@RequestMapping("/api/EmailGroup")
 public class EmailGroupRestController {
     private final EmailGroupService emailGroupService;
 
