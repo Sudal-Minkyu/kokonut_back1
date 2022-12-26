080254c28c34354f11ffe8e86223b84cdad4b3e6
joy <joy@kokonut.me>
2022-12-26 16:27:00 +0900

    불필요한 주석 제거. emailHistoryService를 통해 saveEmailHistory메소드로 가서 저장 처리되던 걸 mailSender내에서 처리하는 로직으로 변경함.

--- src/main/java/com/app/kokonut/joy/email/MailSender.java
+++ src/main/java/com/app/kokonut/joy/email/MailSender.java
@@ -1,8 +1,8 @@
 package com.app.kokonut.joy.email;
 
 
-import com.app.kokonut.email.emailHistory.EmailHistoryService;
-import com.app.kokonut.email.emailHistory.dto.EmailHistoryDto;
+import com.app.kokonut.email.emailHistory.EmailHistoryRepository;
+import com.app.kokonut.email.emailHistory.entity.EmailHistory;
 import com.app.kokonut.navercloud.NaverCloudPlatformService;
 import com.app.kokonut.navercloud.dto.NCloudPlatformMailRequest;
 import com.app.kokonut.navercloud.dto.RecipientForRequest;
@@ -26,12 +26,12 @@ public class MailSender {
     public String myHost;
 
 	private final  NaverCloudPlatformService naverCloudPlatformService;
-	private final EmailHistoryService emailHistoryService;
+	private final EmailHistoryRepository emailHistoryRepository;
 
 	@Autowired
-	public MailSender(NaverCloudPlatformService naverCloudPlatformService, EmailHistoryService emailHistoryService) {
+	public MailSender(NaverCloudPlatformService naverCloudPlatformService, EmailHistoryRepository emailHistoryRepository) {
 		this.naverCloudPlatformService = naverCloudPlatformService;
-		this.emailHistoryService = emailHistoryService;
+		this.emailHistoryRepository = emailHistoryRepository;
 	}
 
 	public boolean sendMail(String toEmail, String toName, String title, String contents) {
@@ -45,7 +45,7 @@ public class MailSender {
 	public boolean sendMail(String fromEmail, String fromName, String toEmail, String toName, String title, String contents) {
 		log.info("### MailSender.sendMail 시작");
 		// 수신자 정보 세팅
-		List<RecipientForRequest> recipients = new ArrayList<RecipientForRequest>();
+		List<RecipientForRequest> recipients = new ArrayList<>();
 		RecipientForRequest recipient = new RecipientForRequest();
 		recipient.setAddress(toEmail);
 
@@ -72,73 +72,20 @@ public class MailSender {
 		if(result) {
 			log.info("### 네이버 클라우드 플랫폼 서비스 sendMail 성공");
 			log.info("### 이메일 발송 내역 저장");
-			EmailHistoryDto emailHistoryDto = new EmailHistoryDto();
-			emailHistoryDto.setFrom(fromEmail);
-			emailHistoryDto.setFromName(fromName);
-			emailHistoryDto.setTo(toEmail);
-			emailHistoryDto.setToName(toName);
-			emailHistoryDto.setTitle(title);
-			emailHistoryDto.setContents(contents);
-			result = emailHistoryService.saveEmailHistory(emailHistoryDto);
-			if(result) {
-				log.info("### 이메일 발송 내역 저장 성공");
-			}else{
-				log.error("### 이메일 발송 내역 저장 실패");
-			}
+			EmailHistory emailHistory = new EmailHistory();
+			emailHistory.setFrom(fromEmail);
+			emailHistory.setFromName(fromName);
+			emailHistory.setTo(toEmail);
+			emailHistory.setToName(toName);
+			emailHistory.setTitle(title);
+			emailHistory.setContents(contents);
 
+			emailHistoryRepository.save(emailHistory);
+			log.info("### 이메일 발송 내역 저장 성공");
 		}else {
 			log.error("### 네이버 클라우드 플랫폼 서비스 sendMail 실패");
 		}
 		return result;
 	}
 
-//	코코넛 기존 소스
-//	public String getHTML(HttpServletRequest request, String viewURL) throws IOException {
-//		String requestURL = request.getRequestURL().toString();
-//		String mailViewURL = requestURL.substring(0, StringUtils.ordinalIndexOf(requestURL, "/", 3)) + viewURL;
-//
-//		URL url = new URL(mailViewURL);
-//		URLConnection conn = url.openConnection();
-//
-//		InputStream is = conn.getInputStream();
-//		return IOUtils.toString(is);
-//	}
-
-//	public String getHTML2(String viewURL) throws IOException {
-//		System.out.println("viewURL : "+viewURL);
-//		String mailViewURL = myHost + viewURL;
-//		System.out.println("mailViewURL : "+mailViewURL);
-
-//		URL url = new URL(mailViewURL);
-//		URLConnection conn = url.openConnection();
-		
-//		InputStream is = conn.getInputStream();
-//		return IOUtils.toString(is);
-//	}
-	
-//	public String getHTML3(HttpServletRequest request, String viewURL) throws IOException {
-		
-//		InetAddress iAddress = InetAddress.getLocalHost();
-//		log.info("### getHTML3 host IP : " + iAddress);
-//		log.info("### getHTML host IP getHostAddress : " + iAddress.getHostAddress());
-		
-//		String mailViewURL = "http://" + iAddress.getHostAddress() + viewURL;
-		
-//		URL url = new URL(mailViewURL);
-//		URLConnection conn = url.openConnection();
-		
-//		InputStream is = conn.getInputStream();
-//		return IOUtils.toString(is);
-//	}
-
-//	public String getHTML4(String viewURL) throws IOException {
-//		String mailViewURL = "http://isms-p-external-elb-1865218457.ap-northeast-2.elb.amazonaws.com" + viewURL;
-		
-//		URL url = new URL(mailViewURL);
-//		URLConnection conn = url.openConnection();
-		
-//		InputStream is = conn.getInputStream();
-//		return IOUtils.toString(is);
-//	}
-	
 }
