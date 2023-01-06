2e47f0fd7094ba3e39d90652e03fde2ec2cb89cb
joy1234 <joy@kokonut.me>
2023-01-06 16:05:35 +0900

    관리자 환경 설정 리팩토링

--- src/main/java/com/app/kokonut/setting/SettingRepositoryCustomImpl.java
+++ src/main/java/com/app/kokonut/setting/SettingRepositoryCustomImpl.java
@@ -1,6 +1,12 @@
 package com.app.kokonut.setting;
 
+import com.app.kokonut.company.CompanyRepository;
+import com.app.kokonut.company.QCompany;
+import com.app.kokonut.refactor.setting.entity.QSetting;
+import com.app.kokonut.setting.dto.SettingDetailDto;
 import com.app.kokonut.setting.entity.Setting;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPQLQuery;
 import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
@@ -13,11 +19,42 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class SettingRepositoryCustomImpl extends QuerydslRepositorySupport implements SettingRepositoryCustom {
+    private final SettingRepository settingRepository;
+    private final CompanyRepository companyRepository;
     public final JpaResultMapper jpaResultMapper;
 
-    public SettingRepositoryCustomImpl(JpaResultMapper jpaResultMapper) {
+    public SettingRepositoryCustomImpl(JpaResultMapper jpaResultMapper,
+                                       CompanyRepository companyRepository,
+                                       SettingRepository settingRepository) {
         super(Setting.class);
         this.jpaResultMapper = jpaResultMapper;
+        this.companyRepository = companyRepository;
+        this.settingRepository = settingRepository;
     }
 
+    @Override
+    public SettingDetailDto findSettingDetailByCompanyIdx(Integer companyIdx) {
+
+        /*
+         *  SELECT A.IDX
+         *       , A.OVERSEAS_BLOCK
+         *       , A.DORMANT_ACCOUNT
+         *    FROM `setting` AS A
+         *    JOIN `company` AS B
+         *      ON A.COMPANY_IDX = B.IDX
+         *   WHERE A.COMPANY_IDX = #{companyIdx}
+         */
+
+        QSetting setting = QSetting.setting;
+        QCompany company = QCompany.company;
+
+        JPQLQuery<SettingDetailDto> query = from(setting)
+                .where(setting.companyIdx.eq(companyIdx))
+                .join(company).on(setting.companyIdx.eq(company.idx))
+                .select(Projections.constructor(SettingDetailDto.class,
+                        setting.idx,
+                        setting.overseasBlock,
+                        setting.dormantAccount));
+        return query.fetchOne();
+    }
 }
--- src/main/java/com/app/kokonut/setting/SettingRestController.java
+++ src/main/java/com/app/kokonut/setting/SettingRestController.java
@@ -1,10 +1,19 @@
 package com.app.kokonut.setting;
 
 
+import com.app.kokonut.auth.jwt.util.SecurityUtil;
+import com.app.kokonut.setting.dto.SettingDetailDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author Joy
@@ -33,4 +42,30 @@ public class SettingRestController {
         this.settingService = settingService;
     }
 
+    @ApiOperation(value="관리자 환경설정 조회", notes="관리자 환경설정 상세 조회")
+    @PostMapping("/settingDetail") // -> 기존의 코코넛 호출 메서드명 : settingsUI, settingsUI - SystemSettingController, MemberSettingController
+    @ApiImplicitParams({@ApiImplicitParam(name ="Bearer", value="JWT Token",required = true, dataTypeClass = String.class, paramType = "header")})
+    public ResponseEntity<Map<String,Object>> settingDetail(){
+        String userRole = SecurityUtil.getCurrentJwt().getRole();
+        String email = SecurityUtil.getCurrentJwt().getEmail();
+        return settingService.settingDetail(userRole, email);
+    }
+
+    @ApiOperation(value="관리자 환경설정 저장", notes="관리자 환경설정 저장")
+    @PostMapping("/settingSave") // -> 기존의 코코넛 호출 메서드명 : /save - MemberSettingController
+    @ApiImplicitParams({@ApiImplicitParam(name ="Bearer", value="JWT Token",required = true, dataTypeClass = String.class, paramType = "header")})
+    public ResponseEntity<Map<String,Object>> settingSave(SettingDetailDto settingDetailDto){
+        String userRole = SecurityUtil.getCurrentJwt().getRole();
+        String email = SecurityUtil.getCurrentJwt().getEmail();
+        return settingService.settingSave(userRole, email, settingDetailDto);
+    }
+
+//    @ApiOperation(value="관리자 환경설정 삭제", notes="관리자 환경설정 전체 삭제")
+//    @PostMapping("/settingDelete")
+//    @ApiImplicitParams({@ApiImplicitParam(name ="Bearer", value="JWT Token",required = true, dataTypeClass = String.class, paramType = "header")})
+//    public ResponseEntity<Map<String,Object>> settingDelete(){
+//        String userRole = SecurityUtil.getCurrentJwt().getRole();
+//        String email = SecurityUtil.getCurrentJwt().getEmail();
+//        return settingService.settingDelete(userRole, email);
+//    }
 }
